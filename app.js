1c50c94be2024fbc0b2dcd7ebc87d5782ba4b451
Chandler Bowman <doctorgrant99@gmail.com>
2023-02-28 19:03:17 -0700

    wrote sendPackage

--- app.js
+++ app.js
@@ -1,75 +1,78 @@
 const packages = [{
-    priorityLevel: 'express',
-    isFragile: false,
-    weight: 2,
-    to: 'Sir Harrington IV',
-    trackingNumber: '1324kjs'
-  },
-  {
-    priorityLevel: 'standard',
-    isFragile: true,
-    weight: .5,
-    to: 'Master Mercutio',
-    trackingNumber: '1325sdk'
-  },
-  {
-    priorityLevel: 'free',
-    isFragile: true,
-    weight: .5,
-    to: 'Mistress Ravenfeather',
-    trackingNumber: 'jffd147'
-  },
-  {
-    priorityLevel: 'standard',
-    isFragile: false,
-    weight: 4,
-    to: 'B. Robert Brown',
-    trackingNumber: 'acdc145'
-  },
-  {
-    priorityLevel: 'express',
-    isFragile: true,
-    weight: 6,
-    to: 'Chancellor Wallace',
-    trackingNumber: '4b2l0z'
-  },
-  {
-    priorityLevel: 'standard',
-    isFragile: false,
-    weight: 5,
-    to: 'Sarah Sharm',
-    trackingNumber: '8081baz'
-  },
-  {
-    priorityLevel: 'free',
-    isFragile: true,
-    weight: 12,
-    to: 'Tae Lien',
-    trackingNumber: 'suz2367'
-  }]
+  priorityLevel: 'express',
+  isFragile: false,
+  weight: 2,
+  to: 'Sir Harrington IV',
+  trackingNumber: '1324kjs'
+},
+{
+  priorityLevel: 'standard',
+  isFragile: true,
+  weight: .5,
+  to: 'Master Mercutio',
+  trackingNumber: '1325sdk'
+},
+{
+  priorityLevel: 'free',
+  isFragile: true,
+  weight: .5,
+  to: 'Mistress Ravenfeather',
+  trackingNumber: 'jffd147'
+},
+{
+  priorityLevel: 'standard',
+  isFragile: false,
+  weight: 4,
+  to: 'B. Robert Brown',
+  trackingNumber: 'acdc145'
+},
+{
+  priorityLevel: 'express',
+  isFragile: true,
+  weight: 6,
+  to: 'Chancellor Wallace',
+  trackingNumber: '4b2l0z'
+},
+{
+  priorityLevel: 'standard',
+  isFragile: false,
+  weight: 5,
+  to: 'Sarah Sharm',
+  trackingNumber: '8081baz'
+},
+{
+  priorityLevel: 'free',
+  isFragile: true,
+  weight: 12,
+  to: 'Tae Lien',
+  trackingNumber: 'suz2367'
+}]
 
-  
-  function drawPackages(){
-    const packageElem = document.getElementById("packages")
-    let template = ''
-    packages.forEach(package => {
-      template += package.trackingNumber
-    });
-    packageElem.innerText = template
-  }
 
-  function drawFragilePackages() {
-    const fragilePackages = packages.filter(x => x.isFragile == true)
-    drawPackages(fragilePackages)
-  }
+function drawPackages(packages) {
+  const packageElem = document.getElementById("packages")
+  let template = ''
+  packages.forEach(package => {
+    template += `${package.trackingNumber}
+      `
+  });
+  packageElem.innerText = template
+}
 
-  function sendPackage() {
-    const sendingElem = document.getElementById("send-package")
-    let randomPackage = Math.floor(Math.random() * packages.length)
-    let sentPackage = packages[randomPackage]
-  }
+function drawFragilePackages() {
+  const fragilePackages = packages.filter(x => x.isFragile === true)
+  drawPackages(fragilePackages)
+}
 
-  function drawAllPackages() {
-    drawPackages(packages)
-  }
+function sendPackage() {
+  const sendingElem = document.getElementById("send-package")
+  let randomPackage = Math.floor(Math.random() * packages.length)
+  let sentPackage = packages[randomPackage]
+  let template = `${sentPackage.trackingNumber} - ${sentPackage.to}`
+  sendingElem.innerText = template
+}
+
+function drawAllPackages() {
+  drawPackages(packages)
+}
 
